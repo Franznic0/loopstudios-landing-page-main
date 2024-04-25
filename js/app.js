de7f3d415831254a355807203c6f502c6f0446b9
Franznic0 <franznicofranz@gmail.com>
2024-04-25 10:33:36 +0100

    mobile menu fix

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 const burger = document.getElementsByClassName("burger");
 const closeButton = document.getElementById("close-button");
-const menu = document.getElementsByClassName("mobile-menu");
+const menu = document.querySelector(".mobile-menu");
 
 burger[0].addEventListener("click", function() {
-    menu[0].style.transform = "translateX(0)";
+    // menu[0].style.transform = "translateX(0)";
+    // menu[0].style.display = "block";
+    menu.classList.toggle('active');
 });
 
 closeButton.addEventListener("click", function() {
-    menu[0].style.transform = "translateX(100%)";
+    // menu[0].style.transform = "translateX(100%)";
+    // menu[0].style.display = "none";
+    menu.classList.toggle('active');
 })
